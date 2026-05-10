cd7748127d6515bcb451e95d36ee730ee1c80cba
Thamaraiselvan Kodhandabani <thko2162@stu.sjcc.edu>
2026-05-09 22:56:45 -0700

    adding order placed component

---
+++ src/app/order-placed/order-placed.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-order-placed',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './order-placed.html',
+  styleUrls: ['./order-placed.css'],
+})
+export class OrderPlaced {}
