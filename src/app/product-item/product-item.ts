60656c6bbd9363538ae4be007b9a0d9e546a265b
Thamaraiselvan Kodhandabani <thko2162@stu.sjcc.edu>
2026-05-09 22:52:47 -0700

    making changes to product-item component

--- src/app/product-item/product-item.ts
+++ src/app/product-item/product-item.ts
@@ -27,7 +27,7 @@ export class ProductItem {
   constructor(
     private cartService: CartService,
     private productsService: ProductsService,
-    private logger: NGXLogger
+    private logger: NGXLogger,
   ) {}
 
   ngOnInit() {
@@ -42,15 +42,15 @@ export class ProductItem {
   }
 
   private generateHash(nameId: string): number {
-    // Generates a repeteable-random non-negative integer value based on
-    // the string s, which is the concatenation of product name and id.
+    let hash: number = 0;
 
-    /*
-     * STUDENTS MUST WRITE CODE FOR THIS FUNCTION
-     */
+    for (let i = 0; i < nameId.length; i++) {
+      const char = nameId.charCodeAt(i);
+      hash = (hash << 5) - hash + char;
+      hash = hash | 0;
+    }
 
-    let hash: number = 2;
-    return hash;
+    return Math.abs(hash);
   }
 
   // Calculates image number for this specific product.
@@ -113,7 +113,7 @@ export class ProductItem {
     const msg = `Unable to add ${this.selected} ${this.prod!.value.name} to cart. ${
       this.prod!.value.currentQuantity
     } are in stock, and ${this.cartService.quantity(
-      this.prod!.value.id
+      this.prod!.value.id,
     )} are already in your cart. You can add up to ${canAdd} more.`;
     this.productsService.message = { show: true, type: ProductAlertType.Danger, text: msg };
     this.logger.error(msg);
